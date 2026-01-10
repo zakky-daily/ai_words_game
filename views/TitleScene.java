02cac75aae72f9327f972cc9962575dbf43fcf45
及川蒼翔 <oikawaaoishou@Aoto.local>
2026-01-10 16:33:09 +0900

    画面遷移とタイトル画面の修正

--- views/TitleScene.java
+++ views/TitleScene.java
@@ -11,32 +11,53 @@ public class TitleScene extends JPanel {
     private JButton wayToPlayButton;
     public TitleScene() {
 
-        this.setSize(640, 680);
+        this.setSize(980, 700);
         
         this.setLayout(null);
         this.setBackground(Color.WHITE);
         
+        // ボタンの追加
+        Font buttonFont = new Font("SansSerif", Font.BOLD, 30);
+        URL imageStart = getClass().getResource("/views/title.png");
+        URL imageToPlay = getClass().getResource("/views/title.png");
+        ImageIcon iconStart = new ImageIcon(imageStart);
+        ImageIcon iconToPlay = new ImageIcon(imageToPlay);
+        // 元の画像サイズが大きすぎる場合、ここでリサイズ
+        Image imgSrtart = iconStart.getImage().getScaledInstance(230, 80, Image.SCALE_SMOOTH);
+        Image imgToPlay = iconToPlay.getImage().getScaledInstance(230, 80, Image.SCALE_SMOOTH);
+
+        // スタートボタンを追加
+        startButton = new JButton();
+        startButton.setBounds(375, 420, 230, 80);
+        startButton.setBackground(new Color(40, 40, 80)); // 濃紺
+        startButton.setForeground(Color.BLACK);
+        startButton.setIcon(iconStart);
+        startButton.setFocusPainted(false);
+        this.add(startButton);
+        
+
+        // 遊び方ボタンを追加
+        wayToPlayButton = new JButton("遊び方");
+        wayToPlayButton.setBounds(375, 540, 230, 80);
+        wayToPlayButton.setForeground(Color.BLACK);
+        wayToPlayButton.setFocusPainted(false);
+        startButton.setIcon(iconToPlay);
+        this.add(wayToPlayButton);
+
         // 背景画像を用意
         URL image = getClass().getResource("/views/title.png");
         if (image != null) {
             ImageIcon icon = new ImageIcon(image);
             // 元の画像サイズが大きすぎる場合、ここでリサイズ
-            Image img = icon.getImage().getScaledInstance(640, 300, Image.SCALE_SMOOTH);
+            Image img = icon.getImage().getScaledInstance(980, 700, Image.SCALE_SMOOTH);
             titleLabel = new JLabel(new ImageIcon(img));
         } else {
+            System.out.println("探した場所: " + getClass().getPackage().getName());
             titleLabel = new JLabel("画像が見つかりません");
         }   
-        titleLabel.setBounds(0, 50, 640, 300);
+        titleLabel.setBounds(0, 0, 980, 700);
         this.add(titleLabel);
 
-        startButton = new JButton("はじめる");
-        startButton.setBounds(350, 400, 230, 80);
-        startButton.setBackground(Color.BLUE);
-        this.add(startButton);
 
-        wayToPlayButton = new JButton("遊び方");
-        wayToPlayButton.setBounds(350, 500, 230, 80);
-        wayToPlayButton.setBackground(Color.BLUE);
-        this.add(wayToPlayButton);
     }
 }
