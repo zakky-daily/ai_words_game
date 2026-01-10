c6ea2dbf1e7dcf32fe44212367dccdcbde15f02a
及川蒼翔 <oikawaaoishou@Aoto.local>
2026-01-10 17:34:52 +0900

    画像を入れるフォルダの更新

--- views/TitleScene.java
+++ views/TitleScene.java
@@ -18,45 +18,42 @@ public class TitleScene extends JPanel {
         
         // ボタンの追加
         // スタートボタンを追加
-        URL imageStart = getClass().getResource("/views/start.png");
+        URL imageStart = getClass().getResource("/start.png");
         if (imageStart != null) {
             ImageIcon iconStart = new ImageIcon(imageStart);
             // ボタンのサイズ(230, 80)に合わせてリサイズ
             Image imgStart = iconStart.getImage().getScaledInstance(230, 80, Image.SCALE_SMOOTH);
             startButton = new JButton(new ImageIcon(imgStart));
+            startButton.setContentAreaFilled(false); // 背景を透明に
+            startButton.setBorderPainted(false);     // 枠線を消す
+            startButton.setFocusPainted(false);      // クリック時の枠を消す
         } else {
             startButton = new JButton("START");
         }
-
         startButton.setBounds(375, 420, 230, 80);
-        // ボタンの装飾を消して画像だけ見せる設定
-        startButton.setContentAreaFilled(false); // 背景を透明に
-        startButton.setBorderPainted(false);     // 枠線を消す
-        startButton.setFocusPainted(false);      // クリック時の枠を消す
-        
         this.add(startButton);
     
 
         // 遊び方ボタンを追加
-        URL imageToPlay = getClass().getResource("/res/howtoplay.png");
+        URL imageToPlay = getClass().getResource("/howtoplay.png");
         if (imageToPlay != null) {
             ImageIcon iconToPlay = new ImageIcon(imageToPlay);
             // 同じく (230, 80) にリサイズ
             Image imgResized = iconToPlay.getImage().getScaledInstance(230, 80, Image.SCALE_SMOOTH);
             wayToPlayButton = new JButton(new ImageIcon(imgResized));
+            wayToPlayButton.setContentAreaFilled(false);
+            wayToPlayButton.setBorderPainted(false);
+            wayToPlayButton.setFocusPainted(false);
         } else {
             wayToPlayButton = new JButton("HOW TO PLAY");
         }
 
         wayToPlayButton.setBounds(375, 540, 230, 80);
-        wayToPlayButton.setContentAreaFilled(false);
-        wayToPlayButton.setBorderPainted(false);
-        wayToPlayButton.setFocusPainted(false);
-        
         this.add(wayToPlayButton);
+        
 
         // 背景画像を用意
-        URL image = getClass().getResource("/res/title.png");
+        URL image = getClass().getResource("/title.png");
         if (image != null) {
             ImageIcon icon = new ImageIcon(image);
             // 元の画像サイズが大きすぎる場合、ここでリサイズ
